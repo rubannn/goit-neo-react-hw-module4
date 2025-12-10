254313c741a52a18da5c508e86b6d4b7599d09b4
m.ruban <m.ruban@testportal.gov.ua>
2025-12-10 14:18:56 +0100

    Wrap photo gallery in a container div for improved layout structure

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,12 +3,14 @@ import css from "./ImageGallery.module.css";
 
 export default function ImageGallery({ items, onClick }) {
   return (
-    <ul className={css["photo-gallery"]}>
-      {items.map((item) => (
-        <li key={item.id} className={css["gallery-item"]}>
-          <ImageCard item={item} onClick={onClick} />
-        </li>
-      ))}
-    </ul>
+    <div className={css["container"]}>
+      <ul className={css["photo-gallery"]}>
+        {items.map((item) => (
+          <li key={item.id} className={css["gallery-item"]}>
+            <ImageCard item={item} onClick={onClick} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
